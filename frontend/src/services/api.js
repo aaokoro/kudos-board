ed2101ce757f42486403ef9ff98938a682584af4
Amaka Amanda Okoro <aaokoro@meta.com>
2025-06-20 15:44:41 -0700

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = import.meta.API_URL;
 
 const createHeaders = (contentType = true) => {
   return contentType ? { 'Content-Type': 'application/json' } : {};
